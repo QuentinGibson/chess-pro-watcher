87b129948ead6e09f8e5d6cd3ef2df1045966200
QuentinGibson <quentingibson94@gmail.com>
2021-06-25 11:35:33 -0400

    refactor table

--- src/App.js
+++ src/App.js
@@ -5,141 +5,33 @@ import chesspgn2 from './database/ct-2842-2762-2020.3.25.pgn'
 import Chess from 'chess.js'
 import { useEffect, useState } from 'react';
 
-const getMovesAsFENs = (chessObj) => {
+const createGame = (pgn) => {
+  const chess = new Chess()
+  chess.load_pgn(pgn)
   const chess1 = new Chess()
-  return chessObj.history().map(function (move) {
+  const fens = chess.history().map(function (move) {
     chess1.move(move);
     return chess1.fen();
   });
+  const headers = chess.header()
+  return {fens, headers}
 }
 
 function App() {
   const [games, setGames] = useState([])
   const [game, setGame] = useState(null)
-  const [player, setPlayer] = useState(null)
-  const [opponent, setOpponent] = useState(null)
-  const [players, setPlayers] = useState([])
   const [turn, setTurn] = useState(0)
-
-  const initializeFirstPlayers = (player) => {
-    let playerGames = games.filter(game => {
-      const {White, Black} = game.headers
-      return (White === player) || (Black === player)
-    })
-    const opponents = playerGames.map(game => {
-      const {White, Black} = game.headers
-      if (White === player) {
-        return (
-          Black
-        )
-      } else {
-        return (
-          White
-        )
-      }
-    })
-    setPlayer(player)
-    setOpponent(opponents[0])
-  }
-
   useEffect(() => {
-    const gameList = [chesspgn, chesspgn2]
-    for (let game of gameList) {
-      const chess = new Chess()
-      chess.load_pgn(game)
-      const fens = getMovesAsFENs(chess)
-      const headers = chess.header()
-      const {White, Black} = headers
-      setPlayers(prevState => {
-        const players = [...prevState]
-        if (!prevState.includes(White)) {
-          players.push(White)
-        }
-        if (!prevState.includes(Black)) {
-          players.push(Black)
-        }
-        return players
-      })
-      initializeFirstPlayers(players[0])
-      setGames(prevState => {
-        if (!prevState.includes({fens, headers}))
-          return [...prevState, { fens, headers }]
-       } )
+    const pgnList = [chesspgn, chesspgn2]
+    for (let pgn of pgnList) {
+      const game = createGame(pgn)
+      setGames(prevState => { return [...prevState, game] } )
     }
   }, [])
-  const chessboard = () => {
-      if (game) {
-        return (
-          <Chessboard position={game.fens[turn]} />
-        )
-      } else {
-        return <Chessboard/>
-      }
-  }
-
-  const playerSelector = () => {
-    const playerOptions = players.map(player => {
+  const MyChessboard = () => {
+    const Controls = () => {
       return (
-        <option value={player}>{player}</option>
-      )
-    })
-    return (
-      <select name="players" id="players" onChange={e => setPlayer(e.target.value)}>
-        {playerOptions}
-      </select>
-    )
-  }
-
-  const opponentSelector = () => {
-    let playerGames = games.filter(game => {
-      const {White, Black} = game.headers
-      return (White === player) || (Black === player)
-    })
-    const opponents = playerGames.map(game => {
-      const {White, Black} = game.headers
-      if (White === player) {
-        return (
-          <option value={Black} >{Black}</option>
-        )
-      } else {
-        return (
-          <option value={White} >{White}</option>
-        )
-      }
-    })
-    return (
-      <select name="opponents" id="opponents" onChange={e => setOpponent(e.target.value)} >
-        {opponents}
-      </select>
-    )
-  }
-
-  const eventSelector = () => {
-    const gamesOfInterest = games.filter(game => {
-      const {White, Black} = game.headers
-      if ((player === White && opponent === Black) || (player === Black && opponent === White)) {
-        return true
-      }
-      return false
-    })
-    const gameOptions = gamesOfInterest.map(game => {
-      return (
-        <option value={game.fens}>{game.headers.Event} - {game.headers.Round}</option>
-      )
-    })
-    return (
-      <select name="games" id="games">
-        {gameOptions}
-      </select>
-    )
-  }
-
-  return (
-    <div className="App">
-      <main className="App-header">
-        <h1 className="text-5xl">Chess Openings Trainer</h1>
         <div>
-          {chessboard()}
           <button onClick={() => setTurn(0)}>Start</button>
           <button
             onClick={() => setTurn(prevState => {
@@ -158,10 +50,62 @@ function App() {
             })}
           >Next</button>
           <button onClick={() => setTurn(game.fens.length - 1)}>End</button>
-          {playerSelector()}
-          {opponentSelector()}
-          {eventSelector()}
-          <button onClick={() => setGame(games[0])}>Set Game</button>
+        </div>
+      )
+    }
+      if (game) {
+        return (
+          <div>
+            <Chessboard position={game.fens[turn]} />
+            <Controls />
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <Chessboard />
+            <Controls />
+          </div>
+        )
+      }
+  }
+  const PGNTable = ({games}) => {
+    const chessTableData = () => {
+      return games.map(game => {
+        const {White, Black, Event, Site, Result} = game.headers
+        return (
+          <tr>
+            <td>{White}</td>
+            <td>{Black}</td>
+            <td>{Event}</td>
+            <td>{Site}</td>
+            <td>{Result}</td>
+            <td><button onClick={() => setGame(game)}>Load Game</button></td>
+          </tr>
+        )
+      })
+    }
+    return (
+      <table>
+        <tr>
+          <th>White</th>
+          <th>Black</th>
+          <th>Event</th>
+          <th>Site</th>
+          <th>Result</th>
+        </tr>
+        {chessTableData()}
+      </table>
+    )
+  }
+
+  return (
+    <div className="App">
+      <main className="App-header">
+        <h1 className="text-5xl">Chess Openings Trainer</h1>
+        <div>
+          <MyChessboard/>
+          <PGNTable games={games}></PGNTable>
         </div>
       </main>
     </div>
