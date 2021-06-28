527ad1b026a25aff742aad7bb3810750370227f9
QuentinGibson <quentingibson94@gmail.com>
2021-06-28 03:43:11 -0400

    feat input of total moves

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Chessboard from 'chessboardjsx';
 import chesspgn from './database/magnus.pgn';
 import chesspgn2 from './database/ct-2842-2762-2020.3.25.pgn'
 import Chess from 'chess.js'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { VscTriangleLeft, VscTriangleRight } from 'react-icons/vsc';
+import {BsSkipEndFill, BsSkipStartFill} from 'react-icons/bs'
 
 const createGame = (pgn) => {
   const chess = new Chess()
@@ -17,53 +19,96 @@ const createGame = (pgn) => {
   return {fens, headers}
 }
 
+const pgnList = [chesspgn, chesspgn2]
+const gameList = pgnList.map(pgn => createGame(pgn))
+
 function App() {
   const [games, setGames] = useState([])
   const [game, setGame] = useState(null)
   const [turn, setTurn] = useState(0)
+  const turnInputRef = useRef();
   useEffect(() => {
-    const pgnList = [chesspgn, chesspgn2]
-    for (let pgn of pgnList) {
-      const game = createGame(pgn)
-      setGames(prevState => { return [...prevState, game] } )
-    }
+    setGames(gameList)
   }, [])
+  useEffect(() => {
+    if (turnInputRef.current){
+      turnInputRef.current.value = turn
+    }
+  }, [turn])
   const MyChessboard = () => {
     const Controls = () => {
       return (
-        <div>
-          <button onClick={() => setTurn(0)}>Start</button>
+        <div className="flex justify-center">
+          <button 
+          className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 rounded border border-gray-800 text-gray-800 px-6 py-2 text-xs" 
+          onClick={() => setTurn(0)}>
+            <BsSkipStartFill style={{margin: '3 3 0 0'}}/>Start
+          </button>
           <button
+          className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 rounded border border-gray-800 text-gray-800 px-6 py-2 text-xs"
             onClick={() => setTurn(prevState => {
               if (prevState > 0) {
                 return prevState - 1
               }
               return prevState
             })}
-          >Previous</button>
+          ><VscTriangleLeft />Previous</button>
           <button
+          className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 rounded border border-gray-800 text-gray-800 px-6 py-2 text-xs"
             onClick={() => setTurn(prevState => {
               if (prevState < game.fens.length - 1) {
                 return prevState + 1
               }
               return prevState
             })}
-          >Next</button>
-          <button onClick={() => setTurn(game.fens.length - 1)}>End</button>
+          ><VscTriangleRight />Next</button>
+          <button 
+          className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 rounded border border-gray-800 text-gray-800 px-6 py-2 text-xs"
+           onClick={() => setTurn(game.fens.length - 1)}>
+             <BsSkipEndFill />End
+          </button>
         </div>
       )
     }
       if (game) {
         return (
-          <div>
-            <Chessboard position={game.fens[turn]} />
+          <div >
+            <div className="flex justify-between">
+              <div className=" border border-gray-300  h-8 w-5/12 mb-4 rounded-md flex items-center justify-center">
+                <div className="flex items-center">
+                  <p className="text-xs text-gray-800 dark:text-gray-400 font-normal">White: {game.headers.White}</p>
+                </div>
+              </div>
+              <div className="border border-gray-300  h-8 w-5/12 bg-gray-900  mb-4 rounded-md flex items-center justify-center">
+                <div className="flex items-center">
+                  <p className="text-xs text-gray-100 dark:text-gray-400 font-normal">Black: {game.headers.Black}</p>
+                </div>
+              </div>
+            </div>
+            <div style={{marginLeft: 250}}>
+              <Chessboard calcWidth={(screenWidth, screenHeight) => 750} position={game.fens[turn]} />
+              <p className="text-gray-800 text-sm font-bold leading-tight tracking-normal mb-2" style={{marginLeft: 320, marginTop: 20}}>
+                Move
+                <input
+                  className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 mx-2 font-normal w-10 h-10 items-center pl-3 text-sm border-gray-300 rounded border shadow"
+                  type="text"
+                  ref={turnInputRef}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      const userInput = parseInt(turnInputRef.current.value)
+                      if (!isNaN(userInput)) setTurn(userInput)
+                    }
+                  }} />
+                of {game.fens.length}
+              </p>
+            </div>
             <Controls />
           </div>
         )
       } else {
         return (
-          <div>
-            <Chessboard />
+          <div >
+            <Chessboard calcWidth={(screenWidth, screenHeight) => 750} />
             <Controls />
           </div>
         )
@@ -71,39 +116,72 @@ function App() {
   }
   const PGNTable = ({games}) => {
     const ChessTableData = () => {
-      return games.map(game => {
+      return games.map((game, index) => {
+        const even = index % 2 === 0
         const {White, Black, Event, Site, Result} = game.headers
+        if (even) {
         return (
-          <tr>
-            <td>{White}</td>
-            <td>{Black}</td>
-            <td>{Event}</td>
-            <td>{Site}</td>
-            <td>{Result}</td>
-            <td><button onClick={() => setGame(game)}>Load Game</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="pb-5 pt-8 pr-20 pl-4">{White}</td>
+              <td className="pb-5 pt-8 pr-20 pl-4">{Black}</td>
+              <td className="pb-5 pt-8 pr-20 pl-4">{Event}</td>
+              <td className="pb-5 pt-8 pr-20 pl-4">{Site}</td>
+              <td className="pb-5 pt-8 pr-20 pl-4">{Result}</td>
+              <td className="pb-5 pt-8 pr-20 pl-4">
+                <button className="text-base font-bold leading-none underline text-center text-gray-900 hover:text-indigo-700" onClick={() => setGame(game)}>
+                  Load Game
+                </button>
+              </td>
+            </tr>
+          </tbody>
         )
+        } else {
+          return (
+            <tbody>
+              <tr className="bg-gray-100">
+                <td className="pb-5 pt-8 pr-20 pl-4">{White}</td>
+                <td className="pb-5 pt-8 pr-20 pl-4">{Black}</td>
+                <td className="pb-5 pt-8 pr-20 pl-4">{Event}</td>
+                <td className="pb-5 pt-8 pr-20 pl-4">{Site}</td>
+                <td className="pb-5 pt-8 pr-20 pl-4">{Result}</td>
+                <td className="pb-5 pt-8 pr-20 pl-4">
+                  <button 
+                    className="text-base font-bold leading-none underline text-center text-gray-900 hover:text-indigo-700"
+                    onClick={() => setGame(game)}>
+                    Load Game
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          )
+        }
       })
     }
     return (
-      <table>
-        <tr>
-          <th>White</th>
-          <th>Black</th>
-          <th>Event</th>
-          <th>Site</th>
-          <th>Result</th>
-        </tr>
+      <table className="w-full whitespace-nowrap border-gray-100 border rounded-lg">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="text-left py-5 pl-4 rounded-tl-lg"><p className="text-sm font-medium leading-none text-gray-900">White</p></th>
+            <th className="text-left py-5 pl-4 rounded-tl-lg"><p className="text-sm font-medium leading-none text-gray-900">Black</p></th>
+            <th className="text-left py-5 pl-4 rounded-tl-lg"><p className="text-sm font-medium leading-none text-gray-900">Event</p></th>
+            <th className="text-left py-5 pl-4 rounded-tl-lg"><p className="text-sm font-medium leading-none text-gray-900">Site</p></th>
+            <th className="text-left py-5 pl-4 rounded-tl-lg"><p className="text-sm font-medium leading-none text-gray-900">Result</p></th>
+            <th className="text-left py-5 pl-4 rounded-tl-lg">
+              <p className="text-sm font-medium leading-none text-gray-900">Play Game</p>
+            </th>
+          </tr>
+        </thead>
         <ChessTableData/>
       </table>
     )
   }
 
   return (
-    <div className="App">
-      <main className="App-header">
-        <h1 className="text-5xl">Chess Pro Watcher</h1>
-        <div>
+    <div className="py-8 flex items-stretch justify-center w-full">
+      <main className="">
+        <h1 className="mb-20 text-7xl md:px-0 px-4 font-medium tracking-widest leading-none text-center text-gray-800 uppercase">Chess Pro Watcher</h1>
+        <div class="flex justify-center flex-col">
           <MyChessboard/>
           <PGNTable games={games}/>
         </div>
