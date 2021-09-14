c6d10dd03365403f268d7768dc74f5e1bbbf9527
QuentinGibson <quentingibson94@gmail.com>
2021-09-14 09:29:12 -0400

    add games

--- src/database/index.js
+++ src/database/index.js
@@ -4,5 +4,11 @@ import chesspgn3 from 'database/game3.pgn'
 import chesspgn4 from 'database/game4.pgn'
 import chesspgn5 from 'database/game5.pgn'
 import chesspgn6 from 'database/game6.pgn'
+import chesspgn7 from 'database/paul-1.pgn'
+import chesspgn8 from 'database/paul-2.pgn'
+import chesspgn9 from 'database/paul-3.pgn'
+import chesspgn10 from 'database/garry-1.pgn'
+import chesspgn11 from 'database/garry-2.pgn'
+import chesspgn12 from 'database/garry-3.pgn'
 
-export default [chesspgn, chesspgn2, chesspgn3, chesspgn4, chesspgn5, chesspgn6]
+export default [chesspgn, chesspgn2, chesspgn3, chesspgn4, chesspgn5, chesspgn6, chesspgn7, chesspgn8, chesspgn9, chesspgn10, chesspgn11, chesspgn12]
